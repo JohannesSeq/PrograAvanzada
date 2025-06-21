bd37dcd366b29af77d52c1535c9b278290f7f9e9
lady17 <leidysalgado17@gmail.com>
2025-06-21 16:06:46 -0600

    Add project files.

---
+++ PlataformadePagos.LogicaN/Interfaces/IComercioService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlataformadePagos.Models;
+
+namespace PlataformadePagos.LogicaN.Interfaces
+{
+    public interface IComercioService
+    {
+        List<ComercioDto> ObtenerTodos();
+        ComercioDto ObtenerPorId(int id);
+        void Registrar(ComercioDto comercio);
+        void Editar(ComercioDto comercio);
+    }
+}
---
+++ PlataformadePagos.Portal/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PlataformadePagos.Portal
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
