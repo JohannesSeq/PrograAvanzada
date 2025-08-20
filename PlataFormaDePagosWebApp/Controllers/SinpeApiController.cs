4b5437af10d52efdbb1f8bd82710d3f9ea2ae0c1
Daraya29 <42704650+Jotadeiru@users.noreply.github.com>
2025-08-20 14:50:41 -0600

    Aplicando vistas

---
+++ PlataFormaDePagosWebApp/Controllers/SinpeApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlataFormaDePagosWebApp.Controllers
+{
+    public class SinpeApiController : Controller
+    {
+        // Vista para consultar SINPEs
+        public ActionResult Consultar()
+        {
+            return View();
+        }
+
+        // Vista para recibir SINPEs
+        public ActionResult Recibir()
+        {
+            return View();
+        }
+
+    }
+}
